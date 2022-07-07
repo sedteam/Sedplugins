d09a7d65e5a77ae30de6b60da0194b465a9f6a15
Seditio Team <amro86@ya.ru>
2022-07-07 18:39:19 +0400

    Add Forum statistics plugin

---
+++ forumstats/forumstats.setup.php
@@ -0,0 +1,33 @@
+<?PHP
+
+/* ====================
+Seditio - Website engine
+Copyright Neocrome
+http://www.neocrome.net
+[BEGIN_SED]
+File=plugins/forumstats/forumstats.setup.php
+Version=178
+Updated=2020-jul-07
+Type=Plugin
+Author=Neocrome
+Description=
+[END_SED]
+
+[BEGIN_SED_EXTPLUGIN]
+Code=forumstats
+Name=Forum statistics
+Description=Statistics for the forums
+Version=178
+Date=2020-jul-07
+Author=Neocrome
+Copyright=
+Notes=
+SQL=
+Auth_guests=R
+Lock_guests=A
+Auth_members=R
+Lock_members=A
+[END_SED_EXTPLUGIN]
+==================== */
+
+?>
---
+++ forumstats/lang/forumstats.ru.lang.php
@@ -0,0 +1,29 @@
+<?PHP
+
+/* ====================
+Seditio - Website engine
+Copyright Neocrome
+http://www.neocrome.net
+[BEGIN_SED]
+File=plugins/lang/forumstats.ru.lang.php
+Version=178
+Updated=2020-jul-07
+Type=
+Author=Neocrome
+Description=
+[END_SED]
+==================== */
+
+
+$L['plu_title'] = "Статистика форума";
+
+$L['plu_posts'] = "Количество сообщений";
+$L['plu_topics'] = "Всего темы";
+$L['plu_sections'] = "Всего разделов";
+$L['plu_views'] = "Общее количество просмотров";
+$L['plu_posterstop10'] = "Авторы Top10";
+$L['plu_viewedtop10'] = "10 самых популярных тема";
+$L['plu_repliedtop10'] = "10 самых активных тем";
+$L['plu_hidden'] = "(скрытый, доступ не допускается)";
+
+?>
